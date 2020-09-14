24d1b33334e8160363ce72386f20532ee657438d
VitalikVoicix <voicix1502@gmail.com>
2020-09-14 23:26:05 +0300

    Updates

---
+++ precache-manifest.edfa3f4a1eb4b38587f8caaa4f706735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976975e1cc240350654c5f9216f8cb39",
+    "url": "/homework17/index.html"
+  },
+  {
+    "revision": "7d2b04674256de81bc56",
+    "url": "/homework17/static/css/main.b6f8e437.chunk.css"
+  },
+  {
+    "revision": "ec607e553db1e799f940",
+    "url": "/homework17/static/js/2.e7544b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework17/static/js/2.e7544b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2b04674256de81bc56",
+    "url": "/homework17/static/js/main.90f3ff7e.chunk.js"
+  },
+  {
+    "revision": "0fc43bb7e03548b73d9f",
+    "url": "/homework17/static/js/runtime-main.06bd00eb.js"
+  }
+]);
